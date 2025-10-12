f885acad984c9274767c1f0b6a4ecf0224592f71
Yolanda O. Amazi <amaziyolanda29@gmail.com>
2025-10-12 17:19:03 +0200

    Update and rename calefactor.ads to P5/calefactor.ads

--- P5/calefactor.ads
+++ P5/calefactor.ads
@@ -1,5 +1,5 @@
 package Calefactor is
-   -- No contemplamos la posibilidad de saturación, es decir,
+   -- No contemplamos la posibilidad de saturaciÃ³n, es decir,
    -- al calefactor se le puede pedir que entregue toda la
    -- potencia que se necesite.
    type Potencias is new Float range 0.0 .. 10_000.0;
