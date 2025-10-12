63bc121aac07aa228b8259966257ae0de3908e28
Yolanda O. Amazi <amaziyolanda29@gmail.com>
2025-10-12 17:23:40 +0200

    Update and rename retardadores.ads to P5/retardadores.ads

--- P5/retardadores.ads
+++ P5/retardadores.ads
@@ -1,7 +1,7 @@
 generic
    Retardo: Float := 1.0; -- Segundos
    type Elementos is private;
-   Tamaño: Positive := 10; -- 10 elementos
+   TamaÃ±o: Positive := 10; -- 10 elementos
 package Retardadores is
    procedure Escribir (el_Elemento: Elementos);
    procedure Leer (un_Elemento: out Elementos);
