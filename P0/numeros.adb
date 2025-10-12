79ad39ff54a6d03258610f53a11f19a0fd3db13f
Yolanda O. Amazi <amaziyolanda29@gmail.com>
2025-10-12 07:42:53 -0700

    Update and rename numeros.adb to P0/numeros.adb

--- P0/numeros.adb
+++ P0/numeros.adb
@@ -1,7 +1,7 @@
 -- Este programa pretende ilustrar la forma de manejo de las herramientas
 -- del entorno de desarrollo en Ada.
 
--- Paquete est醤dar que vamos a manejar.
+-- Paquete est谩ndar que vamos a manejar.
 with Ada.Text_Io; use Ada.Text_Io;
 
 -- Procedimiento principal.
@@ -10,36 +10,36 @@ procedure numeros is
    -- Declaracion de tipos
    type Luz_de_Trafico_t is (Rojo, Ambar, Verde);
 
-   -- Creaci髇 de ejemplares, para la entrada/salida de n鷐eros, a partir
-   -- de los paquete gen閞icos "Ada.Text_Io.Integer_Io" y
+   -- Creaci贸n de ejemplares, para la entrada/salida de n煤meros, a partir
+   -- de los paquete gen茅ricos "Ada.Text_Io.Integer_Io" y
    -- "Ada.Text_Io.Float_Io".
    package Ent_Es is new Ada.Text_Io.Integer_Io(Integer); use Ent_Es;
    package Real_Es is new Ada.Text_Io.Float_Io(Float);
    package Sem_Es is new Ada.Text_Io.Enumeration_IO(Luz_de_Trafico_t);
    
 
-   -- Declaraci髇 de variables locales.
+   -- Declaraci贸n de variables locales.
    I, J: Integer;
    X, Y: Float;
    semaforo: Luz_de_Trafico_t := Rojo;
 
 -- Cuerpo del procedimiento "Numeros".
 begin
-   -- Entrada/salida de n鷐eros enteros.
+   -- Entrada/salida de n煤meros enteros.
    -- Lectura de datos
    New_Line;
-   Put ("Introduce un n鷐ero entero: ");
+   Put ("Introduce un n煤mero entero: ");
    Ent_Es.Get (I);
-   Put ("Introduce otro n鷐ero entero: ");
+   Put ("Introduce otro n煤mero entero: ");
    Ent_Es.Get (J);
    New_Line;
-   -- Presentaci髇 de los resultados en el sistema decimal
+   -- Presentaci贸n de los resultados en el sistema decimal
    Ent_Es.Put (I); Put ('+');
    Ent_Es.Put (J); Put ('=');
    Put (I+J);
    New_Line;
 
-   -- Presentaci髇 de los resultados en binario
+   -- Presentaci贸n de los resultados en binario
    Ent_Es.Put (I, Base => 2);
    Put ('+');
    Ent_Es.Put (J, Base => 2);
@@ -47,7 +47,7 @@ begin
    Ent_Es.Put (I+J, Base => 2);
    New_Line;
 
-   -- Presentaci髇 de los resultados en hexadecimal
+   -- Presentaci贸n de los resultados en hexadecimal
    Ent_Es.Put (I, Base => 16);
    Put ('+');
    Ent_Es.Put (J, Base => 16);
@@ -55,7 +55,7 @@ begin
    Ent_Es.Put (I+J, Base => 16);
    New_Line;
 
-   -- Presentaci髇 de los resultados con formato
+   -- Presentaci贸n de los resultados con formato
    Ent_Es.Put (I);
    Put ('+');
    Ent_Es.Put (J, Width => 0);
@@ -63,18 +63,18 @@ begin
    Ent_Es.Put (I+J, Width => 6);
    New_Line;   New_Line;
 
-   -- Entrada/salida de n鷐eros reales.
+   -- Entrada/salida de n煤meros reales.
    -- Lectura de los datos
-   Put ("Introduce un n鷐ero real: ");
+   Put ("Introduce un n煤mero real: ");
    Real_Es.Get (X);
-   Put ("Introduce otro n鷐ero real: ");
+   Put ("Introduce otro n煤mero real: ");
    Real_Es.Get (Y);
    New_Line;
-   -- Presentaci髇 de los resultados
+   -- Presentaci贸n de los resultados
    Real_Es.Put (X);  Put ('/');  Real_Es.Put (Y); Put ('=');
    Real_Es.Put (X/Y);
    New_Line;  
-   -- Presentaci髇 de los resultados con formato
+   -- Presentaci贸n de los resultados con formato
    Real_Es.Put (X, Fore => 2, Aft => 2, Exp => 3);
    Put ('/');
    Real_Es.Put (Y, Fore => 0, Aft => 0, Exp => 0);
@@ -85,3 +85,4 @@ begin
    -- Mostrar por pantalla el valor del semaforo
    Sem_Es.Put(semaforo);
 end numeros;
+
